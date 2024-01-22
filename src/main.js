0a3bca4db24e88ddb03007a978df87c0c38495f3
Mariana Mozzer Arantes <mozzerm@yahoo.com.br>
2024-01-22 09:56:04 -0300

    Atualização pacotes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
 import router from './router'
+import BootstrapVueNext from 'bootstrap-vue-next'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).use(router).use(store).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+
+const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+app.use(router)
+app.use(BootstrapVueNext)
+app.use(vuetify)
+app.mount('#app')
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container d-flex justify-content-center mt-5">
         <img src="../assets/thalamus-logo.png" alt="" style="height: 500px; opacity: 40%;">
+
+
     </div>
 </template>
 
